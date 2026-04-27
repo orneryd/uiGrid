8cda9b001c06892b1dcec6a1447d864722f4fc53
TJ Sweet <timothyswt@gmail.com>
2026-04-27 16:23:10 -0700

    fix(test,coveralls): fix test coverage reporting and report to coveralls

---
+++ src/app/grid-library-coverage.spec.ts
@@ -0,0 +1,8 @@
+export {};
+
+await import('../../projects/ui-grid/src/lib/grid/grid.api.spec');
+await import('../../projects/ui-grid/src/lib/grid/grid.models.spec');
+await import('../../projects/ui-grid/src/lib/grid/grid.utils.spec');
+await import('../../projects/ui-grid/src/lib/grid/row-searcher.spec');
+await import('../../projects/ui-grid/src/lib/grid/row-sorter.spec');
+await import('../../projects/ui-grid/src/lib/grid/ui-grid.component.spec');
