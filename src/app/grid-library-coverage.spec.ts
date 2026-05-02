df1a95dfebb3a976b4d154098ff733e334b84fc2
TJ Sweet <timothyswt@gmail.com>
2026-05-01 20:04:06 -0700

    fixing angular CI

--- src/app/grid-library-coverage.spec.ts
+++ src/app/grid-library-coverage.spec.ts
@@ -1,8 +1,8 @@
 export {};
 
-await import('../../projects/ui-grid/src/lib/grid/grid.api.spec');
-await import('../../projects/ui-grid/src/lib/grid/grid.models.spec');
-await import('../../projects/ui-grid/src/lib/grid/grid.utils.spec');
-await import('../../projects/ui-grid/src/lib/grid/row-searcher.spec');
-await import('../../projects/ui-grid/src/lib/grid/row-sorter.spec');
+await import('../../projects/ui-grid-core/src/lib/grid.api.spec');
+await import('../../projects/ui-grid-core/src/lib/grid.models.spec');
+await import('../../projects/ui-grid-core/src/lib/grid.utils.spec');
+await import('../../projects/ui-grid-core/src/lib/row-searcher.spec');
+await import('../../projects/ui-grid-core/src/lib/row-sorter.spec');
 await import('../../projects/ui-grid/src/lib/grid/ui-grid.component.spec');
