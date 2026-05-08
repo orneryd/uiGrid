54647de1073928e3576fedda7d60d3de734391bf
orneryd <timothyswt@gmail.com>
2026-05-07 22:58:26 -0700

    reformat clean out any hardcoded viewport heights

--- projects/ui-grid-vanilla/src/index.ts
+++ projects/ui-grid-vanilla/src/index.ts
@@ -18,6 +18,14 @@ export {
   type UiGridControlIconKey,
   type UiGridIconDefinition,
   type UiGridIconOverrides,
+  type FrameworkRenderedSlotsConfig,
+  type FrameworkCellSlot,
+  type FrameworkHeaderSlot,
+  type FrameworkFilterSlot,
+  type FrameworkGroupRowSlot,
+  type FrameworkExpandableRowSlot,
+  type FrameworkEmptyStateSlot,
+  type FrameworkSlotDelta,
 } from './ui-grid-standalone.element';
 
 export { UIGridTemplate } from './components/grid-template';
